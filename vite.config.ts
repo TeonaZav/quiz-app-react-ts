db6d0b73c369de03490e7eb7edfc2f5df902b83c
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-13 19:56:10 +0400

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-ts-quiz/",
+  base: "/quiz-app-react-ts/",
   plugins: [react()],
   server: {
     port: 3000,
